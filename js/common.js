42c7bfb998d34787b00ce2186d3f3e427b6d4c11
Ratibor <klov29@mail.ru>
2018-02-13 22:06:00 +0700

    Начало

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
+
+
+
+});
