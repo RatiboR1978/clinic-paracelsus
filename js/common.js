ea01489533072f4a3b8c6c86cc439ad1f34fa793
Ratibor <klov29@mail.ru>
2018-02-14 16:59:31 +0700

    Делаю contacts

--- js/common.js
+++ js/common.js
@@ -20,3 +20,18 @@ $(function () {
 
 
 });
+
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [56.81934557, 60.61528800],
+        zoom: 17
+    });
+
+    myPlacemark = new ymaps.Placemark([56.81934557, 60.61528800], { hintContent: 'ул. Большакова, д. 68', balloonContent: '' });
+
+    myMap.geoObjects.add(myPlacemark);
+}
