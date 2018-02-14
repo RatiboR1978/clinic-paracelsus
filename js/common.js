c2ef35e2227c9b6be54055c728a55ac5b81b9235
Ratibor <klov29@mail.ru>
2018-02-14 13:03:38 +0700

    Делаю offices

--- js/common.js
+++ js/common.js
@@ -8,6 +8,14 @@ $(function () {
         adaptiveHeight: true
     });
 
+    $('.slider2').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
 
 
 
