0648e8182f6c83b99f8f65c94f8f2f52f278b1ba
Ratibor <klov29@mail.ru>
2018-02-15 11:00:37 +0700

    Начал функционал

--- js/common.js
+++ js/common.js
@@ -3,18 +3,43 @@ $(function () {
     $('.slider').slick({
         dots: true,
         infinite: true,
-        speed: 300,
         slidesToShow: 1,
+        arrows: true,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 6000,
         adaptiveHeight: true
     });
 
     $('.slider2').slick({
         dots: true,
         infinite: true,
-        speed: 300,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 6000,
         slidesToShow: 1,
         adaptiveHeight: true
     });
+
+    if($('.slide1').hasClass("slick-active")) {
+        setTimeout(function () {
+            $('.galleries__description').addClass('galleries__description-active');
+        }, 1000)
+    } else {
+        $('.galleries__description').removeClass('galleries__description-active');
+    }
+
+    $('.slick-arrow').on('click', function () {
+        if($('.slide1').hasClass("slick-active")) {
+            setTimeout(function () {
+                $('.galleries__description').addClass('galleries__description-active');
+            }, 1000)
+        } else {
+            $('.galleries__description').removeClass('galleries__description-active');
+        }
+
+    })
+
 
 
 
